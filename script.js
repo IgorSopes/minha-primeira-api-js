8ad5b281309af8e10ef81319a25cbac005b9616e
IgorSopes <sopesigor@gmail.com>
2024-11-23 22:41:14 -0300

    teste console

---
+++ script.js
@@ -0,0 +1,6 @@
+function mandarMensagem() {
+    console.log(“Estou aprendendo a programar.”);
+    }
+    console.log(“O javascript é legal.”);
+    mandarMensagem();
+    console.log(“Eu gosto de HTML e CSS.”);
