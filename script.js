9019ec6112f21cde34841cc8933b89c32ade9087
IgorSopes <sopesigor@gmail.com>
2024-11-26 15:34:05 -0300

    add var cep

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/').then(resposta => resposta.json()).then(r => console.log(r));
+async function buscaEndereco(cep){
+  try {
+var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+var consultaCepConvertida = await consultaCep.json();
+if(consultaCepConvertida.erro) {
+  throw Error('CEP inválido');
+}
+var cidade = document.getElementById('cidade');
+var logradouro = document.getElementById('endereco');
+var estado = document.getElementById('estado');
 
-
-console.log(consultaCep);
+          console.log(consultaCepConvertida);
+          return consultaCepConvertida;
+ }
+        catch(erro){
+          console.log(erro)
+        }
+        
+ }
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => 
+buscaEndereco(cep.value))
